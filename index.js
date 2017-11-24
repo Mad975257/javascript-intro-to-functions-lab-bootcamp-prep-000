e2e2a46eb33649f42eaaf020480148c09f6454e8
Alejandro Madrigal <madrigalalex88@mail.fresnostate.edu>
2017-11-24 20:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+
+
 function logShout(string) {
-  return string.toUpperCase()
+  console.console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-  return string.toLowerCase()
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
